03ab0be1d6594dc24e40530dd373deb8a932d00d
Paul Kim <paukim59@yahoo.com>
2024-04-02 02:44:49 -0700

    Add functionality to see all search results and add no cover image and update css

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ const db = new pg.Client({
   port: 5432
 });
 
+// Global variables
 const app = express();
 const port = 3000;
+
+// Book search global variables
 const apiBookSearch = "https://openlibrary.org/search.json";
-const apiCoverSearch = "https://covers.openlibrary.org/b/isbn/";
+const apiCoverSearch = "https://covers.openlibrary.org/b/olid/";
 let suggestions = [];
-let currentSearchResults;
+let currentPage = 1;
+let formattedQuery = "";
+let prevAllowed = false;
+let nextAllowed = false;
+let numFound = 0;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -31,25 +38,61 @@ app.get("/search", (req, res) => {
 })
 
 app.post("/search", async (req, res) => {
-  const query = req.body.searchQuery.trim();
-  const formattedQuery = query.replace(/\s+/g, '+');
+  let query = "";
+
+  // Checking if user clicked the button for a new query or a new page of the existing query
+  // New queries need to be trimmed for white space and replace spaces with pluses
+  switch(req.body.search) {
+    case "find":
+      query = req.body.searchQuery.trim();
+      formattedQuery = query.replace(/\s+/g, '+');
+      currentPage = 1;
+      prevAllowed = false;
+      numFound = 0;
+      break;
+    case "prev":
+      --currentPage;
+      if(currentPage === 1) prevAllowed = false;
+      nextAllowed = true;
+      break;
+    case "next":
+      ++currentPage;
+      if(currentPage * 10 >= numFound) nextAllowed = false;
+      prevAllowed = true;
+      break;
+    default:
+      break;
+  }
+
+  // Parameters for the get query to the api
+  // query 1 page of 10 results
   const queryParams = {
-    q: formattedQuery
+    q: formattedQuery,
+    limit: 10,
+    page: currentPage
   };
 
   try {
+    // Reset suggestions array and get request to the book search api
     suggestions = [];
-    const currentSearchResults = await axios.get(apiBookSearch, { params: queryParams });
+    const result = await axios.get(apiBookSearch, { params: queryParams });
+    numFound = result.data.numFound;
+    const bookResults = result.data.docs
 
-    for(let i = 0; i < currentSearchResults.data.numFound; i++) {
-      if(i >= 10) break;
+    // Check if there are enough total results to show a new page of results
+    if(currentPage * 10 >= numFound) nextAllowed = false;
+    else nextAllowed = true;
 
-      const book = currentSearchResults.data.docs[i];
-      const cover = book.isbn[0];
+    // Loop through at most 10 results and add book info to an object which is pushed to suggestions array
+    for(let i = 0; i < bookResults.length; i++) {
+      const book = bookResults[i];
+      const cover = book.cover_edition_key || book.lending_edition_s;
+      let author = "";
+      if(book.author_name) author = book.author_name[0];
       const option = {
         title: book.title,
         subtitle: book.subtitle || "",
-        author: book.author_name[0],
+        author: author,
         cover: apiCoverSearch + cover + "-M.jpg"
       }
 
@@ -57,10 +100,16 @@ app.post("/search", async (req, res) => {
     }
   }
   catch(err) {
-    console.log(err);
+    console.log("Error retrieving data");
   }
 
-  res.render("search.ejs", { searchResults: suggestions });
+  res.render("search.ejs", { 
+    searchResults: suggestions,
+    numResults: suggestions.length,
+    prev: prevAllowed,
+    next: nextAllowed,
+    total: numFound
+  });
 });
 
 app.listen(port, () => {
